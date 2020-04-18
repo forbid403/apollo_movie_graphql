6d9c10b3aefb740bdd77e83cd37f28df70ffa36f
forbid403 <forbid403@naver.com>
2020-04-18 17:33:16 +0900

    [add] suggested movies

--- graphql_react/apollo_movie_graphql/src/routes/Detail.js
+++ graphql_react/apollo_movie_graphql/src/routes/Detail.js
@@ -4,15 +4,20 @@ import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
 import styled from 'styled-components'
 
-const Container = styled.div`
-  height: 100vh;
+const Root = styled.div`
+  height: 100%;
+  width: 100%;
   background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  color: white;
+`;
+const Container = styled.div`
+  height: 85vh;
   width: 100%;
   display: flex;
   justify-content: space-around;
   align-items: center;
-  color: white;
 `;
+
 const Title = styled.div`
   font-size : 65px;
   margin-bottom: 15px;
@@ -20,22 +25,32 @@ const Title = styled.div`
 
 const Poster = styled.div`
   width: 25%;
-  height: 60%;
+  height: 70%;
   background-color: transparent;
   background-image: url(${props => props.bg});
   background-size: cover;
   background-position: center center;
+  border-radius: 7px;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
 `;
-
 
 const Description = styled.p`
   font-size:28px;
 `;
 
-
 const Column = styled.div`
   margin-left: 10px;
   width: 50%;
+`;
+
+const Suggestions = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  justify-content : space-evenly;
+  justify-items : center;
+  width: 100%;
+  height: 15vh;
+  position: relative;
 `;
 
 const GET_MOVIE = gql`
@@ -45,22 +60,35 @@ const GET_MOVIE = gql`
       medium_cover_image
       description_intro
     }
+    suggestions(id: $id){
+      id
+      medium_cover_image
+    }
   }
 `;
 
 export default () => {
-  let {id} = useParams()
+  let { id } = useParams()
   id = parseInt(id)
-  const {loading, data} = useQuery(GET_MOVIE, {
-    variables:{id}
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id }
   })
+
   return (
-    <Container>
-      <Column>
-        <Title>{loading ? "loading..." : data.movie.title}</Title>
-        <Description>{data?.movie?.description_intro}</Description>
-      </Column>
-      <Poster bg={data?.movie?.medium_cover_image}/>
-    </Container>
+    <Root>
+      <Container>
+        <Column>
+          <Title>{loading ? "loading..." : data.movie.title}</Title>
+          <Description>{data?.movie?.description_intro}</Description>
+        </Column>
+        <Poster bg={data?.movie?.medium_cover_image} />
+      </Container>
+      <Suggestions>
+        {data?.suggestions?.map(s =>
+          <Poster key={s.id} bg={s.medium_cover_image} />
+        )}
+      </Suggestions>
+    </Root>
+
   )
 };
